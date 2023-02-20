0386f7c4a2fa1f428ffb5ecb8c5cce1748df349f
rainbowchazer <125148013+rainbowchazer@users.noreply.github.com>
2023-02-21 01:15:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,42 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t i;
+	for (i = 2; i*i <= value; i++) {
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t count = 0;
+  uint64_t result = 1;
+	while (count != n) {
+		result++;
+		if (checkPrime(result) == true)
+			count++;
+	}
+	return result;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	bool fl = false;
+	while (fl == false) {
+		value++;
+		fl = checkPrime(value);
+	}
+	return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0; 
+  uint64_t result = 2;
+	while (result != hbound) {
+		if (checkPrime(result) == true)
+			sum += result;
+		result++;
+	}
+	return sum;
 }
